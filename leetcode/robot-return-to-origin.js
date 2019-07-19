337b413b78ada256f9a969156286bd3390f506e5
lilyhoratio <lilyhzhou@gmail.com>
2019-07-19 16:26:50 -0700

    additional tests for invalid moves and forcing upper case

--- leetcode/robot-return-to-origin.js
+++ leetcode/robot-return-to-origin.js
@@ -5,16 +5,31 @@
  * @return {boolean}
  */
 
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+
 var judgeCircle = function(moves) {
     const movesCount = {
         "U": 0,
         "D": 0,
         "L": 0,
-        "R" :0
+        "R": 0
     }
     
     moves.split("").forEach(move => { //"U"
-        movesCount[move] += 1
+        
+        move = move.toUpperCase(); 
+        
+        console.log(move);
+        
+        if (movesCount[move] !== undefined) {
+            movesCount[move] += 1
+        } else {
+            console.log("invalid move!")
+            return false;
+        }
     })
 
     console.log("moves count obj: ", movesCount)
@@ -22,4 +37,10 @@ var judgeCircle = function(moves) {
     return movesCount["U"] === movesCount["D"] && movesCount["L"] === movesCount["R"] 
 };
 
-// console.log(judgeCircle("UD"))
+console.log(judgeCircle("ud")) // true
+// console.log(judgeCircle("UD")) // true
+// console.log(judgeCircle("UDD")) // false
+// console.log(judgeCircle("LL")) // false
+// console.log(judgeCircle("LR")) // true
+// console.log(judgeCircle("RRU")) // false
+// console.log(judgeCircle("XRRU")) // false
