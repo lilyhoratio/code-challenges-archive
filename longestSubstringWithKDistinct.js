5aa9096d764c01e89529789088a3b57c2df7a7c9
lilyhoratio <lilyhzhou@gmail.com>
2020-04-29 22:40:30 -0700

    longest substring with k distinct

---
+++ longestSubstringWithKDistinct.js
@@ -0,0 +1,33 @@
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+const longest_substring_with_k_distinct = function (str, k) {
+  let maxLength = 0;
+  let characterCount = {};
+  let windowStart = 0;
+
+  for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+    let rightChar = str[windowEnd];
+
+    if (!(rightChar in characterCount)) {
+      characterCount[rightChar] = 0;
+    }
+    characterCount[rightChar] += 1;
+
+    // If we have more than K distinct characters in frequency counter, move windowStart to the right
+    while (Object.keys(characterCount).length > k) {
+      let leftChar = str[windowStart];
+      console.log(leftChar);
+
+      characterCount[leftChar] -= 1; // decrement the number of unique times seen
+      if (characterCount[leftChar] === 0) {
+        delete characterCount[leftChar];
+      }
+      windowStart += 1;
+    }
+    maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+
+  return maxLength;
+};
+
+console.log(longest_substring_with_k_distinct("cbbebi", 3)); // 5
---
+++ smallestSubarrayGivenSum.js
@@ -0,0 +1,30 @@
+// Given an array of positive numbers and a positive number ‘S’,
+// find the length of the smallest contiguous subarray whose sum is
+// greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+// Example 1:
+
+// Input: [2, 1, 5, 2, 3, 2], S=7
+// Output: 2
+// Explanation: The smallest subarray with a sum great than or equal to '7' is [5, 2].
+
+function smallestSubarrayWithGivenSum(arr, targetSum) {
+  let minLength = Infinity;
+  let windowSum = 0;
+  let windowStart = 0;
+
+  for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+    windowSum += arr[windowEnd];
+
+    while (windowSum >= targetSum) {
+      console.log(windowStart, windowEnd, windowSum);
+      minLength = Math.min(windowEnd - windowStart + 1, minLength);
+      windowSum -= arr[windowStart];
+      windowStart++;
+    }
+  }
+
+  return minLength;
+}
+
+console.log(smallestSubarrayWithGivenSum([2, 1, 5, 2, 3, 2], 7)); // 2
