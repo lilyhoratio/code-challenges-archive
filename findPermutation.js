599322dbeaa83b473b8ac7dc663f844a6d03049d
lilyhoratio <lilyhzhou@gmail.com>
2020-05-21 15:25:49 -0700

    practice sliding window

--- findPermutation.js
+++ findPermutation.js
@@ -1,55 +1,108 @@
-// Given a string and a pattern, find out if the string contains any permutation of the pattern.
+// // Given a string and a pattern, find out if the string contains any permutation of the pattern.
+
+// function findPermutation(str, pattern) {
+//   let windowStart = 0;
+//   let matched = 0;
+//   let patternCharFreq = {};
+
+//   for (let char of pattern) {
+//     if (!(char in patternCharFreq)) {
+//       patternCharFreq[char] = 0;
+//     }
+//     patternCharFreq[char] += 1;
+//   }
+//   console.log(patternCharFreq);
+
+//   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//     let rightChar = str[windowEnd];
+
+//     // check if rightChar is in patternCharFreq
+//     //
+//     if (rightChar in patternCharFreq) {
+//       patternCharFreq[rightChar] -= 1;
+
+//       // if the character count in pattern matches that in string, the count of that char will be 0
+//       // when that happens, increment match count by 1
+//       if (patternCharFreq[rightChar] === 0) {
+//         matched += 1;
+//       }
+//     }
+
+//     // if the count of matched characters equals number of chars in pattern, return true
+//     if (Object.keys(patternCharFreq).length === matched) {
+//       return True;
+//     }
+
+//     // shrink window
+//     if (windowEnd >= pattern.length - 1) {
+//       let leftChar = str[windowStart];
+//       windowStart++;
+
+//       if (leftChar in patternCharFreq) {
+//         if (patternCharFreq[leftChar] === 0) {
+//           matched -= 1;
+//         }
+//         patternCharFreq[leftChar] += 1;
+//       }
+//     }
+//   }
+
+//   return false;
+// }
+
+// console.log(findPermutation("odicf", "cd")); // false
+console.log(findPermutation("bcdxabcdy", "bcdyabcdx")); // true
+// console.log(findPermutation("aaacb", "abc")); // true
 
 function findPermutation(str, pattern) {
-  let windowStart = 0;
-  let matched = 0;
-  let patternCharFreq = {};
+  // iterate through pattern and store the counts of each char in hashmap
+
+  const charFreq = {};
 
   for (let char of pattern) {
-    if (!(char in patternCharFreq)) {
-      patternCharFreq[char] = 0;
+    if (!(char in charFreq)) {
+      charFreq[char] = 0;
     }
-    patternCharFreq[char] += 1;
+
+    charFreq[char]++;
   }
-  console.log(patternCharFreq);
+
+  let windowStart = 0;
+  let matched = 0;
+
+  // iterate through string (increment windowEnd and increment windowStart
 
   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
     let rightChar = str[windowEnd];
 
-    // check if rightChar is in patternCharFreq
-    //
-    if (rightChar in patternCharFreq) {
-      patternCharFreq[rightChar] -= 1;
+    if (rightChar in charFreq) {
+      charFreq[rightChar]--;
 
-      // if the character count in pattern matches that in string, the count of that char will be 0
-      // when that happens, increment match count by 1
-      if (patternCharFreq[rightChar] === 0) {
-        matched += 1;
-      }
+      if (charFreq[rightChar] === 0) matched++;
     }
 
-    // if the count of matched characters equals number of chars in pattern, return true
-    if (Object.keys(patternCharFreq).length === matched) {
-      return True;
-    }
+    console.log(matched);
 
-    // shrink window
+    if (matched === Object.keys(charFreq).length) {
+      return true;
+    }
+    // if (matched === pattern.length) {
+    //   return true;
+    // }
+
     if (windowEnd >= pattern.length - 1) {
       let leftChar = str[windowStart];
+
       windowStart++;
 
-      if (leftChar in patternCharFreq) {
-        if (patternCharFreq[leftChar] === 0) {
-          matched -= 1;
+      if (leftChar in charFreq) {
+        if (charFreq[leftChar] === 0) {
+          matched--;
         }
-        patternCharFreq[leftChar] += 1;
+        charFreq[leftChar]++;
       }
     }
   }
 
   return false;
 }
-
-console.log(findPermutation("odicf", "cd")); // false
-// console.log(findPermutation("bcdxabcdy", "bcdyabcdx")); // true
-// console.log(findPermutation("aaacb", "abc")); // true
--- fruitIntoBaskets.js
+++ fruitIntoBaskets.js
@@ -6,41 +6,77 @@
 // You will pick one fruit from each tree until you cannot, i.e.,
 // you will stop when you have to pick from a third fruit type.
 
+// function fruitIntoBaskets(fruits) {
+//   let maxFruits = 0;
+//   const fruitFrequency = {};
+//   let windowStart = 0;
+
+//   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+//     // keep adding new fruit encountered into fruitFrequency, until
+//     // the number of distinct fruit (keys) goes over 2
+
+//     // once number of keys goes over 2, increment windowStart
+//     // then, once you hit 2, re-update maxFruits
+
+//     let rightFruit = fruits[windowEnd];
+
+//     if (!(rightFruit in fruitFrequency)) {
+//       fruitFrequency[rightFruit] = 0;
+//     }
+
+//     fruitFrequency[rightFruit]++;
+
+//     while (Object.keys(fruitFrequency).length > 2) {
+//       let leftFruit = fruits[windowStart];
+
+//       console.log(leftFruit);
+
+//       fruitFrequency[leftFruit] -= 1;
+//       if (fruitFrequency[leftFruit] === 0) delete fruitFrequency[leftFruit];
+
+//       windowStart++;
+//     }
+
+//     maxFruits = Math.max(maxFruits, windowEnd - windowStart + 1);
+//   }
+
+//   return maxFruits;
+// }
+
+console.log(fruitIntoBaskets(["A", "B", "C", "B", "B", "C"])); // 5
+
 function fruitIntoBaskets(fruits) {
+  // k = 2
   let maxFruits = 0;
-  const fruitFrequency = {};
   let windowStart = 0;
+  const fruitFreq = {};
+
+  // add fruit to fruitFreq hashmap
 
   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
-    // keep adding new fruit encountered into fruitFrequency, until
-    // the number of distinct fruit (keys) goes over 2
-
-    // once number of keys goes over 2, increment windowStart
-    // then, once you hit 2, re-update maxFruits
-
     let rightFruit = fruits[windowEnd];
 
-    if (!(rightFruit in fruitFrequency)) {
-      fruitFrequency[rightFruit] = 0;
+    if (!(rightFruit in fruitFreq)) {
+      fruitFreq[rightFruit] = 0;
     }
 
-    fruitFrequency[rightFruit]++;
+    fruitFreq[rightFruit]++;
 
-    while (Object.keys(fruitFrequency).length > 2) {
+    while (Object.keys(fruitFreq).length > 2) {
       let leftFruit = fruits[windowStart];
-
-      console.log(leftFruit);
-
-      fruitFrequency[leftFruit] -= 1;
-      if (fruitFrequency[leftFruit] === 0) delete fruitFrequency[leftFruit];
-
       windowStart++;
+
+      if (fruitFreq[leftFruit]) {
+        fruitFreq[leftFruit]--;
+
+        if (fruitFreq[leftFruit] === 0) {
+          delete fruitFreq[leftFruit];
+        }
+      }
     }
 
-    maxFruits = Math.max(maxFruits, windowEnd - windowStart + 1);
+    maxFruits = Math.max(windowEnd - windowStart + 1, maxFruits);
   }
 
   return maxFruits;
 }
-
-console.log(fruitIntoBaskets(["A", "B", "C", "B", "B", "C"])); // 5
